b9dabec4487fea8262b7c9a509476ed866f9999c
sou1burn <koryakinv_04@mail.ru>
2024-05-20 11:50:10 +0300

    added finding of articulation points

--- tp_graph.cpp
+++ tp_graph.cpp
@@ -72,7 +72,7 @@ bool is_fully_connected_edge_count(std::vector<std::vector<int>> graph) {
 	}
 	//((i != j && graph[i][j] == 1) && (i == j && graph[i][j] == 0))
 	int needed = n * (n - 1) / 2;
-	std::cout << "\n" << count << " " << needed << "\n";
+	//std::cout << "\n" << count << " " << needed << "\n";
 	if (count == needed) {
 		std::cout << "\nGraph is fully connected \n";
 		return true;
@@ -149,7 +149,64 @@ bool is_fully_connected_matrix(std::vector<std::vector<int>>& graph) {
 	return true;
 }
 
+void articulation_dfs(int u, int& time, std::vector<std::vector<int>>& adj_m, std::vector<int>& discovery,
+	std::vector<int>& low, std::vector<int> &parent, std::vector<bool> &ap) {
 
+	int children = 0;
+	discovery[u] = low[u] = ++time;
+	for (int v = 0; v < adj_m.size(); ++v) {
+
+		if (adj_m[u][v] == 1) {
+			if (discovery[v] == -1) {
+				children++;
+				parent[v] = u;
+				articulation_dfs(v, time, adj_m, discovery, low, parent, ap);
+
+				low[u] = std::min(low[u], low[v]);
+				
+				if (parent[u] == -1 && parent[u] > 1) {
+					ap[u] = true;
+				}
+
+				if (parent[u] != -1 && low[v] >= discovery[u]) {
+					ap[u] = true;
+				}
+			}
+			else if (v != parent[u]){
+				low[u] = std::min(low[u], discovery[v]);
+			}
+		}
+	}
+}
+
+void find_articulation_points(std::vector<std::vector<int>> adj_m, int v, std::fstream& out) {
+
+	int n = adj_m.size();
+	std::vector<int> discovery(n, -1);
+	std::vector<int> low(n, -1);
+	std::vector<int> parent(n, -1);
+	std::vector<bool> ap(n, false);
+
+	int time = 0;
+
+	for (int i = 0; i < n; ++i) {
+
+		if (discovery[i] == -1) {
+
+			articulation_dfs(i, time, adj_m, discovery, low, parent, ap);
+		}
+	}
+
+	out << "Articulation points are: \n";
+
+	for (int i = 0; i < n; ++i) {
+		if (ap[i]) {
+			out << i << " ";
+			std::cout << i << " ";
+		}
+	}
+	out << "\n";
+}
 
 int main() {
 
@@ -193,6 +250,7 @@ int main() {
 	std::cout << "\nStart vertex: " << start_vertex << '\n';
 	if (start_vertex >= count) {
 		std::cerr << "Unexisting vertex\n";
+		return -1;
 	}
 
 	fin.close();
@@ -205,8 +263,11 @@ int main() {
 	if (!out.is_open()) {
 		std::cerr << "Error while opening file \n";
 	}
-
+	
+	out << "DFS result: \n";
 	dfs(adj_m, start_vertex, visited, out);
+	out << "\n";
+	find_articulation_points(adj_m, start_vertex, out);
 
 	out.close();
 
